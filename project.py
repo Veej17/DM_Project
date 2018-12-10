1c191e3e3f696a4a4c8fd3c2bf9c6516a71d6307
Veerle <veerlevwinden@hotmail.com>
2018-12-10 10:45:26 +0100

    Import and open module added

--- project.py
+++
@@ -1,8 +0,0 @@
-import scipy.io as sio
-import sklearn.cluster as cl
-import matplotlib.pyplot as plt
-
-From yeast-transcriptomics import SC_expression.csv as expr
-
-for line in expr:
-    print (line)
